c14d37b499cabb15d17f3e4b7e724bcf93f94ad2
adkwazar <40899969+adkwazar@users.noreply.github.com>
2023-09-06 10:46:28 +0200

    Update example_network.py

--- scripts/example_network.py
+++ scripts/example_network.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class AAModel(nn.Module):
     def __init__(self, context_size, HD):
         super().__init__()
-        self.embeddings = nn.Embedding(20, 2) #20 amino acids, each of them represented as 2D point (vector)
+        self.embeddings = nn.Embedding(20, 2) #20 amino acids, each of them represented as 2D point (2D vector)
         self.linear1 = nn.Linear(context_size*2, HD) #context_size = N-1 (notation from paper), HD = number of neurons in a hidden layer
         self.linear2 = nn.Linear(HD, 20) 
 
